a62b1e2cb20bb06c0b0d65f8ecd92522083e6227
sidd8212 <hk24x7.sidd@gmail.com>
2023-02-08 21:45:42 +0000

    chnages done

--- 01200lymdafunctions.py
+++ 01200lymdafunctions.py
@@ -1,5 +1,17 @@
+
 # create lamda function to square a number and print it 
 print("lamda function to square a number and print it " )
 square = lambda x: x * x    # lambda function to square a number    
 print(square(5))
 #   
+
+# calculate squareroot of a number and print it
+# main function for the same 
+def squareroot(x):
+    """Function printing squareroot of the number."""
+    return x ** 0.5
+print("calculate squareroot of a number and print it" )
+
+# lamda for the suqare root 
+squareroot2 = lambda x: x ** 0.5    # lambda function to square a number 
+print(squareroot2(25))
