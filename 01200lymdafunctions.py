78b78d075f6602ef76408edccc09316686d80b49
sidd8212 <hk24x7.sidd@gmail.com>
2023-02-08 21:45:19 +0000

    all chnages

---
+++ 01200lymdafunctions.py
@@ -0,0 +1,5 @@
+# create lamda function to square a number and print it 
+print("lamda function to square a number and print it " )
+square = lambda x: x * x    # lambda function to square a number    
+print(square(5))
+#   
