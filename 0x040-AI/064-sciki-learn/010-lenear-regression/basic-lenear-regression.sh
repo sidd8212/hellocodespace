be6820e9503be8589261184cc92e6b6d39f7d049
sidd8212 <hk24x7.sidd@gmail.com>
2023-02-21 13:27:09 +0000

    ew

---
+++ 0x040-AI/064-sciki-learn/010-lenear-regression/basic-lenear-regression.sh
@@ -0,0 +1,8 @@
+# lenear regression  is based on the assumption that the data is linearly separable 
+# and the data is not noisy. 
+# lenear regression is a supervised learning algorithm.
+# lenear regression is used to predict the value of a continuous variable.
+ # degree of polynomial is 1 
+ # example of lenear regression is predicting the price of a house based on the size of the house.
+ # bigger the house, higher the price.  other factors like location, number of rooms, etc. are not considered.
+ 
