6dd4859116b8529cdb9fd48d4bd1dafd7a78af50
sidd8212 <hk24x7.sidd@gmail.com>
2023-02-22 21:45:38 +0000

    20230221-EOD

---
+++ 0x040-AI/060-IA-basic-theory/00-basics/01-types.sh
@@ -0,0 +1,42 @@
+
+# the saic types of AI are:
+# 1. supervised learning (SL) : 
+#   the AI is trained with a set of data and a set of expected results (labels) for each data point.
+#    The AI is then tested with new data and the expected results are compared to the results obtained by the AI.
+#    The AI is then trained again with the new data and the expected results. 
+#    This process is repeated until the AI is able to predict the expected results with a high accuracy.
+# This type of AI is used in many applications such as: 
+#   - image recognition (e.g. Google Photos)
+#   - speech recognition (e.g. Google Assistant)
+#   - text recognition (e.g. Google Translate)
+#   - handwriting recognition (e.g. Google Keep)
+these solves the problem of "classification" (i.e. the AI is able to classify data into categories) 
+These can also be used to solve the problem of "regression" (i.e. the AI is able to predict a value for a given data point)
+
+
+# 2. unsupervised learning (UL):
+#   the AI is trained with a set of data but no expected results (labels) for each data point.
+#    The AI is then tested with new data and the expected results are compared to the results obtained by the AI.
+#    The AI is then trained again with the new data and the expected results.
+#    This process is repeated until the AI is able to predict the expected results with a high accuracy.
+# This type of AI is used in many applications such as:
+#   - image recognition (e.g. Google Photos)
+#   - speech recognition (e.g. Google Assistant)
+#   - text recognition (e.g. Google Translate)
+#   - handwriting recognition (e.g. Google Keep)
+these solves the problem of "clustering" (i.e. the AI is able to group data into clusters)
+
+# 3. reinforcement learning (RL)
+#   the AI is trained with a set of data and a set of expected results (labels) for each data point.
+#    The AI is then tested with new data and the expected results are compared to the results obtained by the AI.
+#    The AI is then trained again with the new data and the expected results.
+#    This process is repeated until the AI is able to predict the expected results with a high accuracy.
+# This type of AI is used in many applications such as:
+#   - image recognition (e.g. Google Photos)
+#   - speech recognition (e.g. Google Assistant)
+#   - text recognition (e.g. Google Translate)
+#   - handwriting recognition (e.g. Google Keep)
+these solves the problem of "prediction" (i.e. the AI is able to predict the next step in a sequence)
+
+
+
