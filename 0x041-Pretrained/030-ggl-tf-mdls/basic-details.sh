ab8b72ecd554c1f14966a7013a7edf2cd9b5907c
sidd8212 <hk24x7.sidd@gmail.com>
2023-02-23 18:14:08 +0000

    refactored

---
+++ 0x041-Pretrained/030-ggl-tf-mdls/basic-details.sh
@@ -0,0 +1,17 @@
+# details of the basic models 
+# https://www.tensorflow.org/tutorials - basic models
+
+
+# RNN - Recurrent Neural Network - https://www.tensorflow.org/tutorials/recurrent
+# RNN are used for time series data 
+# RNN main use are for text data example  text generation, text classification, text translation, text summarization
+
+# transformers are used for text data
+# Difference between RNN and transformers is that RNN are sequential and transformers are parallel
+# 
+
+# CNN are used for images
+# GAN are used for images
+# Autoencoders are used for images
+# VAE are used for images
+# DQN are used for images
